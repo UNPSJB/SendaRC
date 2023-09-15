9d259b5ca95f95dadffc8b43f4c9a4d059ce0ae7
FernandoB <benitezf.3105@gmail.com>
2023-09-15 15:27:55 -0300

    Factura Config Base

---
+++ factura/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class factura(models.Model):
+    FORMAPAGO = {
+        (1, 'Efectivo'),
+        (2, 'Cheque'),
+        (3, 'Transferencia'),
+    }
+    numfactura = models.IntegerField()
+    importetotal = models.IntegerField()
+    fechaemision = models.DateField(auto_now=True, auto_now_add=False)
+    formapago =  models.PositiveIntegerField(choices=FORMAPAGO)
+    fechapago = models.DateField(auto_now=False, auto_now_add=False, null=True)
+    #DETALLES
