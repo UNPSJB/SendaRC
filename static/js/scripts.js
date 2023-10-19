943695104fe5d9f6e34a5972dc51ca2019585f9d
AgustinBowen <agus_bowen@hotmail.com>
2023-10-18 22:01:22 -0300

    barra del costado expandible

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,31 @@
-$(document).ready(
-  function () {
-    $('.table').DataTable({
-      "pageLength": 8,
-      "lengthChange": false,
+function initializeDataTable() {
+  const table = document.querySelector('.table');
+  if (table) {
+    const dataTable = new DataTable(table, {
+      pageLength: 8,
+      lengthChange: false,
     });
   }
-); //esto hay que mejorarlo
+}
+document.addEventListener("DOMContentLoaded", function () {
+  initializeDataTable();
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+  const navContainer = document.querySelector(".nav-container");
+  const sideBar = document.querySelector(".navbar");
+  const expandButton = document.getElementById("expand-button");
+
+  const isExpanded = localStorage.getItem("isNavExpanded") === "true";
+
+  if (isExpanded) {
+    navContainer.classList.add("expanded");
+    sideBar.classList.add("sideBarExpand");
+  }
+
+  expandButton.addEventListener("click", function () {
+    navContainer.classList.toggle("expanded");
+    sideBar.classList.toggle("sideBarExpand");
+    localStorage.setItem("isNavExpanded", navContainer.classList.contains("expanded"));
+  });
+});
