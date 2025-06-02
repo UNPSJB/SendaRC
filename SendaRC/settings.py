914e2773ae181bbdb71ffd9dff699ab45d58b12b
Agustin Bowen <agus_bowen@hotmail.com>
2025-06-01 21:46:17 -0300

    fix:conectado con supabase

--- SendaRC/settings.py
+++ SendaRC/settings.py
@@ -13,6 +13,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from django.urls import reverse_lazy
 import os
+import dj_database_url 
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -102,10 +103,9 @@ WSGI_APPLICATION = 'SendaRC.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
