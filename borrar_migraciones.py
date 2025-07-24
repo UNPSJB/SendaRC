65ec7c19dfed11bd02ae018189aed1592b0ba204
architin777 <agustinbowen64@gmail.com>
2025-07-24 00:47:51 -0300

    feat: agarrar la url desde el env  en el script para borrar migraciones

--- borrar_migraciones.py
+++ borrar_migraciones.py
@@ -1,5 +1,8 @@
 import os
 import shutil
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 def borrar_contenido(carpeta):
     if os.path.exists(carpeta) and os.path.isdir(carpeta):
@@ -31,7 +34,9 @@ if __name__ == "__main__":
     for carpeta in carpetas_migrations:
         borrar_contenido(carpeta)
 
-    db_path = "database.sqlite3"
+
+
+    db_path = os.getenv("DATABASE_URL")
     if os.path.exists(db_path):
         os.remove(db_path)
         print(f"Base de datos eliminada: {db_path}")
